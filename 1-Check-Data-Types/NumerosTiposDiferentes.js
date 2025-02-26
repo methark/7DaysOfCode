f2206696c16c03debc9b01c47519c3f98f6c23cc
Methark <methark@users.noreply.github.com>
2025-02-26 14:55:30 -0300

    Melhorias.

--- 1-Check-Data-Types/NumerosTiposDiferentes.js
+++ 1-Check-Data-Types/NumerosTiposDiferentes.js
@@ -2,19 +2,37 @@
 // Por Methark - Arthur Braga
 
 //Declarando variáveis globais.
-let numeros = [1, '1', 30, '30', 10, '10', true, "Texto Sem Nexo..."];
+let numeros = [1, '1', 30, '30', 10, '10', true, "Texto Sem Nexo...", 0, false, 2.32, 2];
 let resultado = '';
+
+// Imprime os valores do array números para o usuário.
+resultado += "<b>Lista de valores: " + numeros.toString(); + ".</b><br>"
+document.getElementById("resultado").innerHTML += resultado + "<br>";
 
 for(i = 0; i < numeros.length; i++) // Retorna os resultados no prompt.
 {
     if(typeof numeros[i] === "string")
     {
-        resultado = "A variável '" + numeros[i] + "' é do tipo " + (typeof numeros[i]) + ".<br>";
+        resultado = "A variável '" + numeros[i] + "' é do tipo " + (typeof numeros[i]) + "." + varDif(i, i + 1);
     }
     else
     {
-        resultado = "A variável " + numeros[i] + " é do tipo " + (typeof numeros[i]) + ".<br>";
+        resultado = "A variável " + numeros[i] + " é do tipo " + (typeof numeros[i]) + "." + varDif(i, i + 1);
     }
+
     console.log(resultado);
-    document.getElementById("resultado").innerHTML += resultado;
+    document.getElementById("resultado").innerHTML += resultado.replaceAll(".undefined",".");  
+}
+
+// Verifica se duas variáveis são iguais ou diferentes.
+function varDif(var1, var2)
+{
+    if(numeros[var1] == numeros[var2] && var2 < numeros.length)
+    {
+        return " Os valores " + numeros[var1] + " e " + numeros[var2] + " são iguais.<br>";
+    }
+    else if(var2 < numeros.length)
+    {
+        return " Os valores " + numeros[var1] + " e " + numeros[var2] + " são diferentes.<br>";
+    }
 }
